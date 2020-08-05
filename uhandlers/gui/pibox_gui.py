9a37db291f39373fade65b8594b64b58ce0fcdff
Emerson32 <houndlover32@gmail.com>
2020-08-05 12:17:59 -0400

    Began gui development. Button reattaches all HID devices

--- uhandlers/gui/pibox_gui.py
+++ uhandlers/gui/pibox_gui.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 import gi
 import os
+import threading
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import GLib, Gtk
+
+ATTACH_PIPE_PATH = "/tmp/pibox_attach_pipe"
+ALERT_PIPE_PATH = "/tmp/pibox_alert_pipe"
+
+
+def attach_notify(msg):
+    """ Notifies the main program that the attach button was pressed """
+    with open(ATTACH_PIPE_PATH, "w") as f_obj:
+        f_obj.write(msg)
 
 
 class GridWindow(Gtk.Window):
@@ -14,13 +24,36 @@ class GridWindow(Gtk.Window):
 
         self.attach_button = Gtk.Button(label="Attach HID devices")
         self.attach_button.connect("clicked", self.on_attach_clicked)
-        grid.add(self.attach_button)
+
+        self.alert_label = Gtk.Label(label="")
+
+        grid.attach(self.alert_label, 0, 0, 10, 10)
+        grid.attach(self.attach_button, 0, 15, 10, 10)
 
     def on_attach_clicked(self, widget):
-        print("Hello, world")
+        attach_notify("Reattach")
 
 
-grid_win = GridWindow()
-grid_win.connect("destroy", Gtk.main_quit)
-grid_win.show_all()
-Gtk.main()
+def main():
+
+    grid_win = GridWindow()
+    grid_win.connect("destroy", Gtk.main_quit)
+
+    def update_detection_status(msg):
+        grid_win.alert_label.set_label(msg)
+
+    def detection_listener():
+        while True:
+            with open(ALERT_PIPE_PATH, "r") as f_obj:
+                msg = f_obj.read().strip()
+                if msg == "Attack Detected":
+                    print("Attack Detected")
+
+    grid_win.show_all()
+    alert_thread = threading.Thread(target=detection_listener)
+    alert_thread.start()
+
+
+if __name__ == '__main__':
+    main()
+    Gtk.main()
